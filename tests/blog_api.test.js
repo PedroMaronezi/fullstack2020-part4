36b3bfbccd9d927b8cf78ceb3553afda75de3ee0
Pedro Maronezi <pedromaronezi@usp.br>
2021-01-07 18:13:21 +0100

    4.13 Blog list expansions, step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -93,6 +93,23 @@ describe('POST requests to /api/blogs', () => {
     })
 })
 
+describe('DELETE requests', () => {
+    test('Delete an existing blog', async () => {
+        let response = await helper.blogsInDb()
+        const blogToDelete = response[0]
+
+        await api 
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        response = await helper.blogsInDb()
+        expect(response).toHaveLength(helper.initialBlogs.length - 1)
+
+        const titles = response.map(blog => blog.title)
+        expect(titles).not.toContain(blogToDelete.title)
+    })
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
