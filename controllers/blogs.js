c945a67c25879ed99ec4c10893351c1b179ec4aa
Pedro Maronezi <pedromaronezi@usp.br>
2021-01-08 18:59:17 +0100

    fix PUT request

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -52,7 +52,8 @@ blogsRouter.put('/:id', async (request, response) => {
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: body.userId
     }
 
     const options = {
@@ -61,7 +62,7 @@ blogsRouter.put('/:id', async (request, response) => {
         context: 'query'
     }
 
-    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id , newBlog, options)
+    const updatedBlog = await Blog.findByIdAndUpdate(request.params.id , newBlog, options).populate('user', { username: 1, name: 1 })
     response.json(updatedBlog)
 })
 
